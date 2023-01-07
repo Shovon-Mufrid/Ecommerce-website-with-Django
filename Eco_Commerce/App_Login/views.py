706c186cf56756f772ef7884c9bd2eee419af640
Mia Md Mufrid Shovon <shovonmufrid98@gmail.com>
2023-01-07 17:22:17 +0600

    Profile create

--- Eco_Commerce/App_Login/views.py
+++ Eco_Commerce/App_Login/views.py
@@ -9,6 +9,8 @@ from django.contrib.auth import login,logout,authenticate
 # form and model
 from App_Login.models import Profile
 from App_Login.forms import ProfileForm, SignUpForm
+# Message
+from django.contrib import messages #error, success, info, warning
 
 # SIGN UP
 def sign_up(request):
@@ -17,6 +19,7 @@ def sign_up(request):
         form =  SignUpForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Account Created Successfully")
             return HttpResponseRedirect(reverse('App_Login:login'))
     return render(request, 'App_Login/signup.html', context={'form':form})        
 
@@ -38,7 +41,22 @@ def login_user(request):
 @login_required
 def logout_user(request):
      logout(request)
+     messages.warning(request, "You are Logged Out")
      return HttpResponse("Logged Out")
+
+# Profile 
+@login_required
+def user_profile(request):
+    profile = Profile.objects.get(user=request.user)
+    form = ProfileForm(instance=profile)
+    if request.method == "POST":
+        form=ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile Change Saved!")
+
+            form=ProfileForm(instance=profile)
+    return render(request, 'App_Login/change_profile.html', context={'form': form})        
 
 
 
