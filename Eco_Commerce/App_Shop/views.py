1d864ad836f9e2961a6c287670e32637b3c7327f
Mia Md Mufrid Shovon <shovonmufrid98@gmail.com>
2023-01-07 18:21:24 +0600

    Product

--- Eco_Commerce/App_Shop/views.py
+++ Eco_Commerce/App_Shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
 # Create your views here.
+from App_Shop.models import Product 
+# Import Views
+from django.views.generic import ListView, DetailView
+
+class Home(ListView):
+    model = Product
+    template_name = 'App_Shop/home.html'
+    # by default context value is : object_list
