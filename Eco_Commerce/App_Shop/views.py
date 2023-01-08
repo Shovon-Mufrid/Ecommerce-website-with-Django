21107a4935a74a6db30d412905eef052f1939dde
Mia Md Mufrid Shovon <shovonmufrid98@gmail.com>
2023-01-08 16:46:10 +0600

    Feature Product add

--- Eco_Commerce/App_Shop/views.py
+++ Eco_Commerce/App_Shop/views.py
@@ -3,8 +3,17 @@ from django.shortcuts import render
 from App_Shop.models import Product 
 # Import Views
 from django.views.generic import ListView, DetailView
+#mixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 class Home(ListView):
     model = Product
     template_name = 'App_Shop/home.html'
     # by default context value is : object_list
+
+
+class ProductDetail(DetailView, LoginRequiredMixin):
+    model = Product
+    template_name = 'App_Shop/product_details.html'  
+        # by default context value is : object
